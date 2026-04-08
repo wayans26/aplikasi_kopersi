7e49513c81448ae8a1e8c4a003d6c01fd455b56f
wayans26 <wayan.setiawan98@gmail.com>
2026-04-08 10:11:02 +0800

    add database

---
+++ app/Models/daftar_rekening.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class daftar_rekening extends Model
+{
+    //
+    protected $connection = 'mysql';
+    protected $table = 'files';
+    // public $incrementing = true;
+    protected $primaryKey = 'id';
+    protected $keyType = 'string';
+    protected $fillable = [
+        'id',
+        'name',
+        'path',
+        'type',
+        'extension',
+    ];
+}
